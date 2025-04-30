21c83b4b8c48211e839683af5ca550763fc5af2b
Jackson <czheng34@ucsc.edu>
2025-04-30 13:48:00 -0700

    the login test update

--- Apps/driver-parking/test/ui/login.test.tsx
+++ Apps/driver-parking/test/ui/login.test.tsx
@@ -1,4 +1,4 @@
-import { it, afterEach } from 'vitest'
+import { it, afterEach, vi } from 'vitest'
 import { render, screen, cleanup } from '@testing-library/react'
 
 import Login from '../../src/app/Login/View'
@@ -6,6 +6,14 @@ import Login from '../../src/app/Login/View'
 afterEach(() => {
   cleanup()
 })
+
+vi.mock('../../src/app/login/action', () => ({
+  signup: vi.fn()
+}))
+
+vi.mock('next/navigation', () => ({
+  useRouter: vi.fn()
+}))
 
 it('should render the Login in component', async () => {
   render(<Login />)
