0e7264f0c7146664bff9b31d0cbf9974c25e5f2a
Matthew Solomon <fsolomon@ucsc.edu>
2025-04-29 21:27:07 -0700

    SQL files and docker compose

---
+++ App/VehicleService/sql/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO vehicle (id, owner, data) VALUES (
+  '93b35c24-3ad2-4ec9-8404-f50d8e357ace',
+  '45c90975-92e0-4a51-b5ea-2fe5f8613b54',
+  jsonb_build_object(
+    'license_plate', '1XXX000',
+    'make', 'Jeep',
+    'model', 'Trailhawk',
+    'color', 'cyan'
+  )
+);
