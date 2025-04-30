1e58daaf42922e356679b07ac9f671ed8a57e373
Bodie <bfeinber@ucsc.edu>
2025-04-30 13:14:00 -0700

    Reorganized Folder structure

--- Microservices/AuthService/vite.config.ts
+++ Microservices/AuthService/vite.config.ts
@@ -1,11 +1,11 @@
-import {defineConfig, configDefaults} from 'vitest/config';
+import { defineConfig, configDefaults } from 'vitest/config.js';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   test: {
     environment: 'node',
-    exclude:[
-      ...configDefaults.exclude, 
+    exclude: [
+      ...configDefaults.exclude,
       'build/*'
     ],
     coverage: {
