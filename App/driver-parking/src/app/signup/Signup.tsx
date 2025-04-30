47723e09641f4a0b0b68685fbbc6dbedac4ec2aa
Jennifer <jchen566@ucsc.edu>
2025-04-29 22:44:47 -0700

    Signup.tsx tested #2

--- App/driver-parking/src/app/signup/Signup.tsx
+++ App/driver-parking/src/app/signup/Signup.tsx
@@ -29,8 +29,6 @@ export default function Signup() {
       case 'password':
         setPassword(value)
         break
-      default:
-        break
     }
   }
 
--- App/driver-parking/test/ui/Signup.test.tsx
+++ App/driver-parking/test/ui/Signup.test.tsx
@@ -1,13 +1,79 @@
-import { it, afterEach } from 'vitest'
-import { render, screen, cleanup } from '@testing-library/react'
+import { it, afterEach, vi, expect } from 'vitest'
+import { render, screen, cleanup, fireEvent } from '@testing-library/react'
+import { useRouter } from 'next/navigation'
 
 import Signup from '../../src/app/signup/Signup'
+import { signup } from '../../src/app/signup/actions'
 
 afterEach(() => {
   cleanup()
+  localStorage.clear()
+  vi.restoreAllMocks()
 })
 
-it('should render the Signup component', async () => {
+vi.mock('../../src/app/signup/actions', () => ({
+  signup: vi.fn()
+}))
+
+vi.mock('next/navigation', () => ({
+  useRouter: vi.fn()
+}))
+
+const mockSignup = signup as ReturnType<typeof vi.fn>
+
+it('mocks successful Signup process', async () => {
+  const mockPush = vi.fn();
+
+  vi.mocked(useRouter).mockReturnValue({ push: mockPush } as any)
+
+  mockSignup.mockResolvedValueOnce({ name: 'Test User' })
+
   render(<Signup />)
-  await screen.findByText('Create an Account')
+
+  const email = screen.getByLabelText('Email').querySelector('input')
+  const name = screen.getByLabelText('Name').querySelector('input')
+  const password = screen.getByLabelText('Password').querySelector('input')
+  const signup = screen.getByLabelText('Sign Up')
+
+  if (!email || !name || !password) {
+    throw new Error('Input(s) not found')
+  }
+
+  fireEvent.change(email, {target: { value: 'example@email.com' }})
+  fireEvent.change(name, {target: { value: 'Test User' }})
+  fireEvent.change(password, {target: { value: 'password' }})
+  fireEvent.click(signup)
+
+  await vi.waitFor(() => {
+    expect(mockPush).toHaveBeenCalledWith('/')
+  })
+})
+
+it('mocks unsuccessful Signup process', async () => {
+  const mockPush = vi.fn();
+
+  vi.mocked(useRouter).mockReturnValue({ push: mockPush } as any)
+  mockSignup.mockResolvedValueOnce(undefined)
+  const alertMock = vi.spyOn(window, 'alert').mockImplementation(() => {});
+
+
+  render(<Signup />)
+
+  const email = screen.getByLabelText('Email').querySelector('input')
+  const name = screen.getByLabelText('Name').querySelector('input')
+  const password = screen.getByLabelText('Password').querySelector('input')
+  const signup = screen.getByLabelText('Sign Up')
+
+  if (!email || !name || !password) {
+    throw new Error('Input(s) not found')
+  }
+  
+  fireEvent.change(email, {target: { value: 'example@email.com' }})
+  fireEvent.change(name, {target: { value: 'Test User' }})
+  fireEvent.change(password, {target: { value: 'password' }})
+  fireEvent.click(signup)
+
+  await vi.waitFor(() => {
+    expect(alertMock).toHaveBeenCalledWith('This email has already been taken.')
+  })
 })
