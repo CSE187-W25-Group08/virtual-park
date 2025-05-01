8a516c35b8cfb4b06b781333bd275cb851e953b6
Bodie <bfeinber@ucsc.edu>
2025-04-30 21:14:37 -0700

    Added checker with connection auth service in vehicle service #58

--- Microservices/VehicleService/src/auth/checker.ts
+++ Microservices/VehicleService/src/auth/checker.ts
@@ -1,14 +1,17 @@
+import { SessionUser } from '../types/express'
+
 import { AuthChecker } from "type-graphql"
 import { Request } from "express"
 
-import { SessionUser } from '../../types/express'
-
+import { AuthService } from "./service"
 
 export const expressAuthChecker: AuthChecker<Request> = async (
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  { root, args, context, info }, roles) => 
-{
-  // placeholder
-  context.user = {id : '45c90975-92e0-4a51-b5ea-2fe5f8613b54'} as SessionUser
+  { root, args, context, info },) => {
+  try {
+    context.user = await new AuthService().check(context.headers.authorization)
+  } catch {
+    return false
+  }
   return true
 }
