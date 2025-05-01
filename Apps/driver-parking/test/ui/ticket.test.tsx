5c0fd5b7fd8fe4a653f41e8f00102271bc0eb7ff
Jin Le <jiqle@ucsc.edu>
2025-04-30 17:59:53 -0700

    added tests to ticket to check for deductible price

--- Apps/driver-parking/test/ui/ticket.test.tsx
+++ Apps/driver-parking/test/ui/ticket.test.tsx
@@ -2,14 +2,28 @@ import { it, afterEach, expect } from 'vitest'
 import { render, screen, cleanup} from '@testing-library/react'
 
 import TicketList from '../../src/app/ticket/list'
-// import { signup } from '../../src/app/signup/actions'
 
 afterEach(() => {
   cleanup()
 })
 
-it('Expect ticketlist to contain', async () => {
+it('contains Violation Text', async () => {
     render(<TicketList/>)
     const violationTexts = screen.getAllByText('Violation');
     expect(violationTexts).toHaveLength(2);
+})
+
+it('contains Paid Text', async () => {
+    render(<TicketList/>)
+    screen.getByText('Paid');
+})
+
+it('contains Unpaid Text', async () => {
+    render(<TicketList/>)
+    screen.getByText('Unpaid');
+})
+
+it('contains ticket deductible', async () => {
+    render(<TicketList/>)
+    await screen.findByText('$50.24');
 })
