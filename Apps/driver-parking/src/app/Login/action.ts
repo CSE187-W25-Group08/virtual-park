008fb9fdc97d17cf6e48e7e93b72e3561e46ca57
Jackson <czheng34@ucsc.edu>
2025-04-30 17:07:55 -0700

    change

--- Apps/driver-parking/src/app/Login/action.ts
+++ Apps/driver-parking/src/app/Login/action.ts
@@ -4,13 +4,14 @@ import { cookies } from 'next/headers'
 
 import { Credentials, Authenticated } from '../../auth'
 import {authenticate} from '../../auth/service'
-// import {encrypt} from '../../auth/jwtAuth'
+import {encrypt} from '../../auth/jwtAuth'
 
 export async function login(credential: Credentials) : Promise<Authenticated|undefined> {
   const user = await authenticate(credential)
   if (user) {
     const expiresAt = new Date(Date.now() + 30 * 60 * 1000)
-    const session = user.accessToken
+    const session = await encrypt(user.accessToken)
+    // const session = user.accessToken
     const cookieStore = await cookies()
    
     cookieStore.set('session', session, {
--- Apps/driver-parking/src/app/page.tsx
+++ Apps/driver-parking/src/app/page.tsx
@@ -4,13 +4,15 @@ import { Fragment, useState } from 'react';
 import { Button, Typography } from '@mui/material';
 
 // import RegisterVehicle from './Register/Register'
-import Vehicles from './register/Vehicles'
+import Vehicles from './Register/Vehicles'
+import Login from './Login/page'
 
 export default function Home() {
   return (
     <Fragment>
       <Typography>Welcome to Virtual-Park!</Typography>
       <Vehicles/>
+      <Login/>
     </Fragment>
   );
 }
