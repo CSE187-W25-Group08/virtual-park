13ac93fc72189ec4a45175d2e5ae7283cd39cedf
Hamza <hbassal@ucsc.edu>
2025-05-02 02:51:02 -0700

    registering vehicle in UI is not connected to MicroService. still need tests. #5

--- Apps/driver-parking/src/app/Register/actions.ts
+++ Apps/driver-parking/src/app/Register/actions.ts
@@ -2,10 +2,15 @@
 
 import { cookies } from 'next/headers'
 
-import { Vehicle } from '../../register'
+import { Vehicle, VehicleForm } from '../../register'
 import { RegisterService } from '../../register/service'
 
 export async function getUserVehicles() : Promise<Vehicle[]> {
   const cookie = (await cookies()).get('session')?.value
   return new RegisterService().getUserVehicles(cookie)
+}
+
+export async function registerVehicle(vehicle: VehicleForm) : Promise<Vehicle> {
+  const cookie = (await cookies()).get('session')?.value
+  return new RegisterService().registerVehicle(cookie, vehicle)
 }
