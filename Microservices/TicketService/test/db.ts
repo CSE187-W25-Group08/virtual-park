0b67a5b0829f9204a4cc5a596f778e9e9a07e688
Matthew Solomon <fsolomon@ucsc.edu>
2025-05-03 02:15:01 -0700

    Added test databases.sql and modified unit composes

--- Microservices/TicketService/test/db.ts
+++ Microservices/TicketService/test/db.ts
@@ -7,7 +7,7 @@ process.env.POSTGRES_DB = 'test'
 
 const pool = new Pool({
   host: 'localhost',
-  port: 5434,
+  port: 5432,
   database: process.env.POSTGRES_DB,
   user: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
