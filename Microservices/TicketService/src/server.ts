fd56049330022e875c51743e6ecc24faba24b9ee
Matthew Solomon <fsolomon@ucsc.edu>
2025-05-03 22:42:44 -0700

    Now running GraphQL APIs on different ports

--- Microservices/TicketService/src/server.ts
+++ Microservices/TicketService/src/server.ts
@@ -3,8 +3,8 @@ dotenv.config()
 
 import { app, bootstrap } from './app'
 
-app.listen(4000, async () => {
+app.listen(4010, async () => {
   await bootstrap()
-  console.log('Running GraphQL Task Service at http://localhost:4000')
-  console.log('GraphQL Playground: http://localhost:4000/playground')
+  console.log('Running GraphQL Task Service at http://localhost:4010')
+  console.log('GraphQL Playground: http://localhost:4010/playground')
 })
--- Microservices/VehicleService/src/server.ts
+++ Microservices/VehicleService/src/server.ts
@@ -3,8 +3,8 @@ dotenv.config()
 
 import { app, bootstrap } from './app'
 
-app.listen(4000, async () => {
+app.listen(4020, async () => {
   await bootstrap()
-  console.log('Running GraphQL Task Service at http://localhost:4000')
-  console.log('GraphQL Playground: http://localhost:4000/playground')
+  console.log('Running GraphQL Task Service at http://localhost:4020')
+  console.log('GraphQL Playground: http://localhost:4020/playground')
 })
