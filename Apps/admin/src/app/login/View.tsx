7f119bc5a65bc76376c4d9f5406584524b55da8e
Bodie <bfeinber@ucsc.edu>
2025-05-04 18:10:12 -0700

    added login error message #73

--- Apps/admin/src/app/login/View.tsx
+++ Apps/admin/src/app/login/View.tsx
@@ -4,16 +4,17 @@ import React from 'react'
 import { useState,ChangeEvent } from 'react'
 import { useRouter } from 'next/navigation'
 import { 
-  Container, 
   Typography, 
   Box, 
   Button, 
-  TextField
+  TextField,
+  Alert
 } from '@mui/material'
 import { login } from './action'
 
 export default function LoginView() {
   const [credentials, setCredentials] = useState({email: '', password: ''})
+  const [failedLogin, setFailedLogin] = useState(false)
   const router = useRouter()
 
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
@@ -30,20 +31,26 @@ export default function LoginView() {
   const handleClick = async () => {
     const authenticated = await login(credentials)
     if (authenticated) {
+      setFailedLogin(false);
       window.sessionStorage.setItem('name', authenticated.name)
       router.push('/')
+    } else {
+      setFailedLogin(true);
     }
   }
 
   return (
-    <Container
-      maxWidth="xs"
+    <Box
       sx={{
+        mt: 20,
+        width: '100%',
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
       }}
     >
+      {failedLogin &&
+            <Alert severity="error">Incorrect login credentials, please try again</Alert>}
       <Box
         sx={{
           display: 'flex',
@@ -52,7 +59,7 @@ export default function LoginView() {
           mt: 4,
         }}
       >
-        <Typography variant="h5">
+        <Typography variant="h3">
           {'Virtual-Park Admin Login'}
         </Typography>
         
@@ -108,6 +115,6 @@ export default function LoginView() {
           </Button>
         </Box>
       </Box>
-    </Container>
+    </Box>
   )
 }
--- Apps/admin/test/combined/Login.test.tsx
+++ Apps/admin/test/combined/Login.test.tsx
@@ -58,3 +58,35 @@ it('should call login and redirect on valid credentials', async () => {
     expect(mockPush).toHaveBeenCalledWith('/')
   })
 })
+
+it('Login denies invalid credentials', async () => {
+  const mockPush = vi.fn()
+  vi.mocked(useRouter).mockReturnValue({ push: mockPush } as any)
+
+  // Mock fetch to simulate both login + check API
+  vi.mocked(fetch).mockImplementation((url, options) => {
+    if (url?.toString().includes('/login')) {
+      return Promise.resolve({
+        status: 401,
+        json: () => Promise.resolve(undefined),
+      } as Response)
+    }
+    // if (url?.toString().includes('/check')) {
+    //   return Promise.resolve({
+    //     status: 200,
+    //     json: () => Promise.resolve({}),
+    //   } as Response)
+    // }
+    return Promise.reject('Unknown fetch')
+  })
+
+  render(<Page />)
+
+  await userEvent.type(screen.getByPlaceholderText('Email Address'), 'anna@books.com')
+  await userEvent.type(screen.getByPlaceholderText('Password'), 'annaadmin')
+  await userEvent.click(screen.getByText('Sign in'))
+
+  await vi.waitFor(() => {
+    expect(mockPush).toHaveBeenCalledWith('/')
+  })
+})
