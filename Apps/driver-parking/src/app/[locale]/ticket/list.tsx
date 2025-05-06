765a3c41123102c48ffdb1e98df1fd6174a56ee3
Hamza <hbassal@ucsc.edu>
2025-05-05 18:03:49 -0700

    rearranged the ticket list page (simplistic)

--- Apps/driver-parking/src/app/[locale]/ticket/list.tsx
+++ Apps/driver-parking/src/app/[locale]/ticket/list.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { Box, Typography } from "@mui/material";
+import { AppBar, Box, Toolbar, Typography } from "@mui/material";
 import TicketCard from "./card";
 import { Ticket } from "../../../ticket";
 import { listPaid, listUnpaid } from "./actions";
@@ -24,33 +24,56 @@ export default function TicketList() {
     fetchData();
   }, []);
 
-  const TableHeader = (title: string) => (
+  const TableHeader = (title: string, type: string) => (
     <Box
       sx={{
+        mt: 7,
         display: "flex",
         flexDirection: "row",
         justifyContent: "space-between",
       }}
     >
-      <Typography>Violation</Typography>
+      {/* <Typography>Violation</Typography> */}
       <Typography>{title}</Typography>
       <Box>
-        <Typography>Deductible</Typography>
-        <Typography>Issue Date</Typography>
+        {/* <Typography>Deductible</Typography> */}
+        {type == "unpaid" ? 
+        (<Typography>Issue Date</Typography>) :
+        (<Typography>Date Paid</Typography>)}
       </Box>
     </Box>
   );
 
   return (
-    <List>
-      {TableHeader("Unpaid")}
-      {unpaidTicket.map((ticket, index) => (
-        <TicketCard key={index} ticket={ticket} />
-      ))}
-      {TableHeader("Paid")}
-      {paidTicketList.map((ticket, index) => (
-        <TicketCard key={index} ticket={ticket} />
-      ))}
-    </List>
+    <>
+      <AppBar>
+        <Toolbar>
+          <Typography>Tickets</Typography>
+        </Toolbar>
+      </AppBar>
+      
+      <Box sx={{ px: 2 }}>
+        <List sx={{ width: '100%' }}>
+          {unpaidTicket.length <= 0 ?
+          <Typography sx={{mt: 7}}>No current violations on file.</Typography> : (
+            <>
+              {TableHeader("🔴 Unpaid Violations", "unpaid")}
+              {unpaidTicket.map((ticket, index) => (
+                <TicketCard key={index} ticket={ticket} />
+              ))}
+            </>
+          )}
+
+          {paidTicketList.length > 0 && (
+            <>
+              {TableHeader("✅ Paid Violations", "paid")}
+              {paidTicketList.map((ticket, index) => (
+                <TicketCard key={index} ticket={ticket} />
+              ))}
+            </>
+          )}
+        </List>
+      </Box>
+    </>
   );
 }
