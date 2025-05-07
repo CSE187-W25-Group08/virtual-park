b7fbcdbbc4e2df94bd1d127e9804288bcfdaaa8a
Jennifer <jchen566@ucsc.edu>
2025-05-06 19:37:16 -0700

    add failed login/signup alerts for driver app

--- Apps/driver-parking/src/auth/service.ts
+++ Apps/driver-parking/src/auth/service.ts
@@ -1,43 +1,48 @@
 import { NewUser, Authenticated, Credentials} from './'
 
 export async function signupUser(user: NewUser): Promise<Authenticated|undefined> {
-  return new Promise((resolve, reject) => {
-    fetch('http://localhost:3010/api/v0/auth/signup', {
+  try {
+    const response = await fetch('http://localhost:3010/api/v0/auth/signup', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(user),
     })
-    .then(response => { 
-      if (response.status != 201) {
-        reject(response.statusText)
-      }
-      return response.json()} 
-    )
-    .then(data => resolve(data))
-    .catch(error => reject(error))
-  })
+
+    if (response.status != 201) {
+      throw new Error(response.statusText)
+    }
+
+    const data: Authenticated = await response.json();
+    return data;
+  } catch (err) {
+    console.error("Signup failed:", err)
+    return undefined
+  }
 }
 
 export async function authenticate(credentials: Credentials): Promise<Authenticated|undefined> {
-  return new Promise((resolve, reject) => {
-    fetch('http://localhost:3010/api/v0/auth/login', {
+  try {
+    const response = await fetch('http://localhost:3010/api/v0/auth/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(credentials),
     })
-    .then(response => { 
-      if (response.status != 200) {
-        reject(response.statusText)
-      }
-      return response.json()} 
-    )
-    .then(data => resolve(data))
-    .catch(error => reject(error))
-  })
+
+    if (response.status != 200) {
+      throw new Error(response.statusText)
+    }
+
+    const data: Authenticated = await response.json();
+
+    return data;
+  } catch (err) {
+    console.error("Authentication failed:", err)
+    return undefined
+  }
 }
 
 export async function check(cookie: string|undefined): Promise<void> {
