c8dce7303b1050141683ee402bebf71244294505
Jackson <czheng34@ucsc.edu>
2025-05-07 12:50:58 -0700

    update on the enforcement app issue#93

---
+++ Apps/enforcement/src/app/permit/validPermit/page.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+import ValidPermitList from './validPermitList'
+
+const TypePage: NextPage = () => {
+  return (
+    <ValidPermitList/>
+  )
+}
+
+export default permitPage
