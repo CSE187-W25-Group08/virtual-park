7071c5d51496fa50d8ae92aa8f1fb26c4b7c4797
Jackson <czheng34@ucsc.edu>
2025-05-07 12:47:41 -0700

    update on the enforcement app issue#93

---
+++ Apps/enforcement/src/app/permit/action.ts
@@ -0,0 +1,12 @@
+'use server'
+
+import { cookies } from 'next/headers'
+
+
+import { Permit } from '../../../permit'
+import { getPermitByDriver, getPermitType } from '../../../permit/service'
+
+export async function getUserPermits() : Promise<Permit[]> {
+  const cookie = (await cookies()).get('session')?.value
+  return getPermitByDriver(cookie)
+}
---
+++ Apps/enforcement/src/permit/index.ts
@@ -0,0 +1,6 @@
+export interface Permit {
+  issueDate: string,
+  expDate: string,
+  type: string,
+  price: number
+}
