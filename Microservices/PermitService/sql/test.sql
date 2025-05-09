a316d886662546e0f59dad597df04c99bae109a5
Jackson <czheng34@ucsc.edu>
2025-05-08 22:11:33 -0700

    clean up of the data.sql of permitservice

--- Microservices/PermitService/sql/test.sql
+++ Microservices/PermitService/sql/test.sql
@@ -40,24 +40,24 @@ SELECT
     'issue_date', '2025-03-06T08:00:00.000Z',
     'exp_date', '2025-03-06T09:00:00.000Z'
   )
-FROM permitType
-WHERE data->>'type' = 'Staff';
+-- FROM permitType
+-- WHERE data->>'type' = 'Staff';
 
-select * from permitType;
+-- select * from permitType;
 
-select * from driverpermit
-where driverid = 'bea45ed8-aa83-4c49-a201-4625baa0e91a'
+-- select * from driverpermit
+-- where driverid = 'bea45ed8-aa83-4c49-a201-4625baa0e91a'
 
 
-select permitInfo.permitID, permitInfo.permitType, permitInfo.issueDate, permitInfo.expDate, permitInfo.isValid
-from vehicle v
-LEFT JOIN 
-  dblink('dbname=permit user=postgres',
-    $$select dp.driverID AS driverID, dp.id AS permitID, pt.data->>'type' AS permitType, dp.data->>'issue_date' AS issueDate,
-      dp.data->>'exp_date' AS expDate, (dp.data->>'exp_date')::timestamp > NOW() AS isValid
-      from driverpermit dp 
-      join permittype pt ON dp.permitType = pt.id $$)
-      AS permitInfo( driverID UUID, permitID UUID, permitType TEXT,issueDate TEXT, expDate TEXT, isValid BOOLEAN ) 
-      ON v.driver = permitInfo.driverID
-      where 
-      v.data->>'license_plate' = '123BC4A';
+-- select permitInfo.permitID, permitInfo.permitType, permitInfo.issueDate, permitInfo.expDate, permitInfo.isValid
+-- from vehicle v
+-- LEFT JOIN 
+--   dblink('dbname=permit user=postgres',
+--     $$select dp.driverID AS driverID, dp.id AS permitID, pt.data->>'type' AS permitType, dp.data->>'issue_date' AS issueDate,
+--       dp.data->>'exp_date' AS expDate, (dp.data->>'exp_date')::timestamp > NOW() AS isValid
+--       from driverpermit dp 
+--       join permittype pt ON dp.permitType = pt.id $$)
+--       AS permitInfo( driverID UUID, permitID UUID, permitType TEXT,issueDate TEXT, expDate TEXT, isValid BOOLEAN ) 
+--       ON v.driver = permitInfo.driverID
+--       where 
+--       v.data->>'license_plate' = '123BC4A';
