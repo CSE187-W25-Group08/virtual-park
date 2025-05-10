81f96cfedc8d964d02ad9bb456a431611c722334
Bodie <bfeinber@ucsc.edu>
2025-05-10 00:21:01 -0700

    activeAppeals Service

--- Apps/admin/src/ticket/service.ts
+++ Apps/admin/src/ticket/service.ts
@@ -2,7 +2,7 @@ import { Ticket } from "."
 
 export class TicketService {
 
-  public async getPaidTicket(cookie: string|undefined): Promise<Ticket[]>  {
+  public async getPaidTicket(cookie: string | undefined): Promise<Ticket[]> {
     return new Promise((resolve, reject) => {
       fetch('http://localhost:4010/graphql', {
         method: 'POST',
@@ -10,22 +10,23 @@ export class TicketService {
           'Content-Type': 'application/json',
           'Authorization': `Bearer ${cookie}`,
         },
-        body: JSON.stringify({query: `{paidTicket {id, vehicle, enforcer, lot, paid, description, due, issue, violation, image, cost, appeal}}`}),
+        body: JSON.stringify({ query: `{paidTicket {id, vehicle, enforcer, lot, paid, description, due, issue, violation, image, cost, appeal}}` }),
       })
-      .then(response => { 
-        if (response.status != 200) {
-          reject('Unauthorized')
+        .then(response => {
+          if (response.status != 200) {
+            reject('Unauthorized')
+          }
+          return response.json()
         }
-        return response.json()} 
-      )
-      .then(json => {
-        resolve(json.data.paidTicket)
-      })
-      .catch(() => reject('Unauthorized'))
+        )
+        .then(json => {
+          resolve(json.data.paidTicket)
+        })
+        .catch(() => reject('Unauthorized'))
     })
   }
 
-  public async getUnpaidTicket(cookie: string|undefined): Promise<Ticket[]>  {
+  public async getUnpaidTicket(cookie: string | undefined): Promise<Ticket[]> {
     return new Promise((resolve, reject) => {
       fetch('http://localhost:4010/graphql', {
         method: 'POST',
@@ -33,18 +34,44 @@ export class TicketService {
           'Content-Type': 'application/json',
           'Authorization': `Bearer ${cookie}`,
         },
-        body: JSON.stringify({query: `{unpaidTicket {id, vehicle, enforcer, lot, paid, description, due, issue, violation, image, cost, appeal}}`}),
+        body: JSON.stringify({ query: `{unpaidTicket {id, vehicle, enforcer, lot, paid, description, due, issue, violation, image, cost, appeal}}` }),
       })
-      .then(response => { 
-        if (response.status != 200) {
-          reject('Unauthorized')
+        .then(response => {
+          if (response.status != 200) {
+            reject('Unauthorized')
+          }
+          return response.json()
         }
-        return response.json()} 
-      )
-      .then(json => {
-        resolve(json.data.unpaidTicket)
-      })
-      .catch(() => reject('Unauthorized'))
+        )
+        .then(json => {
+          resolve(json.data.unpaidTicket)
+        })
+        .catch(() => reject('Unauthorized'))
     })
   }
+
+  public async getActiveAppeals(cookie: string | undefined): Promise<Ticket[]> {
+    return new Promise((resolve, reject) => {
+      fetch('http://localhost:4010/graphql', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${cookie}`,
+        },
+        body: JSON.stringify({ query: `{activeAppeals {id, vehicle, enforcer, lot, paid, description, due, issue, violation, image, cost, appeal}}` }),
+      })
+        .then(response => {
+          if (response.status != 200) {
+            reject('Unauthorized')
+          }
+          return response.json()
+        }
+        )
+        .then(json => {
+          resolve(json.data.unpaidTicket)
+        })
+        .catch(() => reject('Unauthorized'))
+    })
+  }
+
 }
