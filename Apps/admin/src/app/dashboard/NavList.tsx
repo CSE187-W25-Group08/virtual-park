e2062c070a4aebaf95833b38dea2e8c60de991b2
Bodie <bfeinber@ucsc.edu>
2025-05-12 14:46:51 -0700

    Added proper routing to nav #99

--- Apps/admin/src/app/dashboard/NavList.tsx
+++ Apps/admin/src/app/dashboard/NavList.tsx
@@ -8,25 +8,38 @@ import {
 } from'@mui/material'
 import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
 import PeopleRoundedIcon from '@mui/icons-material/PeopleRounded';
+import { useState } from 'react';
+import { usePathname, useRouter } from 'next/navigation';
+
 export default function NavList() {
-  // MUI example https://github.com/mui/material-ui/blob/v7.1.0/docs/data/material/getting-started/templates/dashboard/components/MenuContent.tsx
   const listItems = [
     { text: 'Home', page: "/", icon: <HomeRoundedIcon /> },
     { text: 'Drivers', page: "/drivers", icon: <PeopleRoundedIcon /> },
   ];
-  const nav = (page: string) => {
-    console.log('redirecting to', page);
-  }
+
+  const router = useRouter();
+  const pathname = usePathname();
+  const currentIndex = listItems.findIndex(item => item.page === pathname);
+  const [selectedIndex, setSelectedIndex] = useState(currentIndex >= 0 ? currentIndex : 0);
+
+  const handleClick = (index: number, page: string) => {
+    setSelectedIndex(index);
+    router.push(page);
+  };
+  // MUI example https://github.com/mui/material-ui/blob/v7.1.0/docs/data/material/getting-started/templates/dashboard/components/MenuContent.tsx
   return (
     <List dense>
-    {listItems.map((item, index) => (
-      <ListItem key={index} disablePadding sx={{ display: 'block' }}>
-        <ListItemButton selected={index === 0} onClick={() => nav(item.page)}>
-          <ListItemIcon>{item.icon}</ListItemIcon>
-          <ListItemText primary={item.text} />
-        </ListItemButton>
-      </ListItem>
-    ))}
-  </List>
-  )
+      {listItems.map((item, index) => (
+        <ListItem key={index} disablePadding sx={{ display: 'block' }}>
+          <ListItemButton
+            selected={index === selectedIndex}
+            onClick={() => handleClick(index, item.page)}
+          >
+            <ListItemIcon>{item.icon}</ListItemIcon>
+            <ListItemText primary={item.text} />
+          </ListItemButton>
+        </ListItem>
+      ))}
+    </List>
+  );
 }
