332f85c2165f4739019871c5ead9eb5f7ce4e44d
Hamza <hbassal@ucsc.edu>
2025-05-13 00:19:54 -0700

    fix lint

--- Apps/driver-parking/src/app/[locale]/permit/purchase/Typelist.tsx
+++ Apps/driver-parking/src/app/[locale]/permit/purchase/Typelist.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState, useEffect } from 'react'
-import { useTranslations } from 'next-intl'
+// import { useTranslations } from 'next-intl'
 
 import { permitTypes } from '../actions'
 import PermitCard from './Typecard'
@@ -9,7 +9,7 @@ import { PermitType } from '../../../../permit'
 
 export default function TypeList() {
   const [permitTypeList, setpermitTypeList] = useState<PermitType[]>([])
-  const t = useTranslations('purchase_permit')
+  // const t = useTranslations('purchase_permit')
   // const [purchased, setPurchased] = useState<Permit | null>(null)
 
   useEffect(() => {
