9f89cb4969fb90fc3072ae6f33eac57923705d9d
Jackson <czheng34@ucsc.edu>
2025-05-13 16:45:52 -0700

    update on the VehicleTest

--- Microservices/VehicleService/test/vehicle.test.ts
+++ Microservices/VehicleService/test/vehicle.test.ts
@@ -107,3 +107,76 @@ test('user who do no thave primary car', async () => {
     })
 })
 
+test('user who has primary car', async () => {
+  await supertest(server)
+    .post('/graphql')
+    .set('Authorization', `Bearer Placeholder`)
+    .send({
+      query: `
+        mutation {
+          registerVehicle(input: {
+            licensePlate: "TEST123",
+            make: "Toyota",
+            model: "Corolla",
+            color: "Silver"
+          }) {
+            id
+            licensePlate
+            make
+            model
+            color
+            driver
+            active
+          }
+        }
+      `
+    })
+  await supertest(server)
+    .post('/graphql')
+    .set('Authorization', `Bearer Placeholder`)
+    .send({
+      query: `{
+        primaryVehicle {
+          id
+          driver
+          licensePlate
+          make
+          model
+          color
+          active
+        }
+      }`
+    })
+    .then((res) => {
+      console.log('primary vehicle info:',res.body.data.primaryVehicle)
+      expect(res.body.data.primaryVehicle.licensePlate).toBe("TEST123")
+    })
+})
+
+
+
+// test('user gets primary vehicle', async () => {
+//   const accessToken = await getAccessToken(molly.email, molly.password);
+//   const result = await supertest(server)
+//     .post('/graphql')
+//     .set('Authorization', `Bearer ${accessToken}`)
+//     .send({
+//       query: `{
+//         primaryVehicle {
+//           id
+//           driver
+//           licensePlate
+//           make
+//           model
+//           color
+//           active
+//         }
+//       }`
+//     });
+//   expect(result.body.data.primaryVehicle.licensePlate).toBe('TEST123');
+// });
+
+// function then(arg0: (res: any) => void) {
+//   throw new Error('Function not implemented.')
+// }
+
