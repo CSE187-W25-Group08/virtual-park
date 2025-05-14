cfe1749bd5960e8ba4bb941052f9507a93b351a2
Matthew Solomon <fsolomon@ucsc.edu>
2025-05-13 22:41:35 -0700

    Adjusted the public API routes and added a default root route

--- APIs/CampusPolice/src/server.ts
+++ APIs/CampusPolice/src/server.ts
@@ -5,5 +5,5 @@ import app from './app'
 
 app.listen(3020, () => {
   console.log('Server running on port 3020')
-  console.log('API Testing UI: http://localhost:3020/api/v0/police/docs')
+  console.log('API Testing UI: http://localhost:3020/v0/police/docs')
 })
