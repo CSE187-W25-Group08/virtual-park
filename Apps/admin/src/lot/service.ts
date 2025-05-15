15614c77ec24c0a675c83715420619bd2263b411
Jin Le <jiqle@ucsc.edu>
2025-05-15 09:20:43 -0700

    added implmentation of putid service to call proper graphql query

--- Apps/admin/src/lot/service.ts
+++ Apps/admin/src/lot/service.ts
@@ -24,10 +24,26 @@ export class LotService {
     })
   }
 
+  // https://chatgpt.com/c/68261342-a478-800d-99f0-15b9a2aa6b13 conversion from Object to GraphQL string
+  private helperObjToGraphql(obj: UpdateLotData) {
+    return `{${Object.entries(obj)
+      .map(([key, value]) => {
+        if (typeof value === 'string') {
+          // Escape double quotes and backslashes inside strings
+          const escaped = value.replace(/\\/g, '\\\\').replace(/"/g, '\\"');
+          return `${key}: "${escaped}"`;
+        }  else {
+          return `${key}: ${value}`;
+        }
+      })
+      .join(', ')}}`;
+  }
+
   public async updateLots(lotId: string, data: UpdateLotData): Promise<Lot> {
 
-    const jsonString = JSON.stringify(data);
+    const jsonString = this.helperObjToGraphql(data)
     console.log(jsonString);
+
 
     return new Promise((resolve, reject) => {
       fetch('http://localhost:4040/graphql', {
@@ -36,7 +52,7 @@ export class LotService {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({ query: `mutation {
-          putId(id: "${lotId}", data: {name: "Dragon", zone: "North"}) {
+          putId(id: "${lotId}", data: ${jsonString}) {
             id,
             name,
             zone, 
