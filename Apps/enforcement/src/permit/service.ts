007fa5fee88c38365552d842449df5354c8a4f98
Jackson <czheng34@ucsc.edu>
2025-05-15 14:11:06 -0700

    update on the service of the enforcement app for issue 93

--- Apps/enforcement/src/permit/service.ts
+++ Apps/enforcement/src/permit/service.ts
@@ -1,6 +1,6 @@
 import {Permit} from '.'
 
-export async function getPermitByDriver(cookie: string | undefined):Promise<Permit[]> {
+export async function getPermitByCarPlate(cookie: string | undefined):Promise<Permit[]> {
   return new Promise((resolve, reject) => {
     fetch('http://localhost:4000/graphql', {
       method: 'POST',
@@ -8,7 +8,7 @@ export async function getPermitByDriver(cookie: string | undefined):Promise<Perm
         'Content-Type': 'application/json',
         'Authorization': `Bearer ${cookie}`,
       },
-      body: JSON.stringify({query: `{permitsByDriver {issueDate, expDate, type, price}}`}),
+      body: JSON.stringify({query: `{getPermitBycarPlate {permitID, permitType, issueDate, expDate, isValid}}`}),
     })
     .then(response => { 
       if (response.status != 200) {
