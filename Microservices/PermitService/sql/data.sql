77af966a6516a20f35de51b5a9874654cae8ac69
Jennifer <jchen566@ucsc.edu>
2025-05-16 00:01:52 -0700

    add new permit types to db #118

--- Microservices/PermitService/sql/data.sql
+++ Microservices/PermitService/sql/data.sql
@@ -24,6 +24,38 @@ INSERT INTO permitType (id, data) VALUES (
   )
 );
 
+INSERT INTO permitType (id, data) VALUES (
+  '9b968eea-9abe-457c-ae79-1b128074f683',
+  jsonb_build_object(
+    'price', 5,
+    'type', 'Daily'
+  )
+);
+
+INSERT INTO permitType (id, data) VALUES (
+  '8616e7a7-bd6e-45e2-9809-ed22c727a6da',
+  jsonb_build_object(
+    'price', 27,
+    'type', '1 Week'
+  )
+);
+
+INSERT INTO permitType (id, data) VALUES (
+  '7acb1a82-c27a-4440-ace7-6d47add695dd',
+  jsonb_build_object(
+    'price', 90,
+    'type', '1 Month'
+  )
+);
+
+INSERT INTO permitType (id, data) VALUES (
+  '5ed85022-ec19-4e22-aff8-9a98feddeea9',
+  jsonb_build_object(
+    'price', 515.95,
+    'type', '1 Year'
+  )
+);
+
 INSERT INTO driverPermit (id, driverID, permitType, data) VALUES (
   '1978402d-6fb5-4515-8bcf-612d99bd9657',
   'bea45ed8-aa83-4c49-a201-4625baa0e91a',
