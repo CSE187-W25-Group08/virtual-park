4786710c81813040cad2b9042e05a867ed2a9ca0
Jackson <czheng34@ucsc.edu>
2025-05-22 15:44:36 -0700

    update on the issue 122

--- Apps/enforcement/src/lot/service.ts
+++ Apps/enforcement/src/lot/service.ts
@@ -1,48 +1,5 @@
 import { Lot } from './index'
 
-// export async function getAllLots(cookie: string | undefined): Promise<Lot[]> {
-//   return new Promise((resolve, reject) => {
-//     fetch('http://localhost:4040/graphql', {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json',
-//         'Authorization': `Bearer ${cookie}`,
-//       },
-//       body: JSON.stringify({
-//         query: `
-//           query getallLots {
-//             getAll {
-//               id
-//               name
-//               zone
-//               address
-//               latitude
-//               longitude
-//               capacity
-//               availableSpots
-//               isActive
-//               type
-//               created
-//               updated
-//             }
-//           }
-//         `,
-//       }),
-//     })
-//     .then(response => {
-//       if (response.status !== 200) {
-//         reject('Unauthorized')
-//         return
-//       }
-//       return response.json()
-//     })
-//     .then(json => {
-//       resolve(json.data.getAll)
-//     })
-//     .catch((error) => reject(error))
-//   })
-// }
-
 export async function getAllLots(cookie: string | undefined): Promise<Lot[]> {
   return new Promise((resolve, reject) => {
     fetch('http://localhost:4040/graphql', {
@@ -53,7 +10,7 @@ export async function getAllLots(cookie: string | undefined): Promise<Lot[]> {
       },
       body: JSON.stringify({
         query: `
-          query GetAllLots {
+          query getallLots {
             getAll {
               id
               name
@@ -73,7 +30,6 @@ export async function getAllLots(cookie: string | undefined): Promise<Lot[]> {
       }),
     })
     .then(response => {
-      console.log('Response status:', response.status)
       if (response.status !== 200) {
         reject('Unauthorized')
         return
@@ -81,35 +37,8 @@ export async function getAllLots(cookie: string | undefined): Promise<Lot[]> {
       return response.json()
     })
     .then(json => {
-      console.log('GraphQL Response:', JSON.stringify(json, null, 2))
-      
-      if (!json) {
-        reject('Empty response from server')
-        return
-      }
-      
-      if (json.errors) {
-        console.error('GraphQL Errors:', json.errors)
-        reject(json.errors[0].message || 'GraphQL Error')
-        return
-      }
-      
-      if (!json.data) {
-        reject('No data field in response')
-        return
-      }
-      
-      if (!json.data.getAll) {
-        reject('No getAll field in response data')
-        return
-      }
-      
       resolve(json.data.getAll)
     })
-    .catch((error) => {
-      console.error('Fetch error:', error)
-      reject(error)
-    })
+    .catch((error) => reject(error))
   })
 }
-
