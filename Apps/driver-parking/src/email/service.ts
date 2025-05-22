8112fb6a7841dfbd106b61c7d3ce211c31acf716
Jennifer <jchen566@ucsc.edu>
2025-05-21 19:43:14 -0700

    add skeleton #127

--- Apps/driver-parking/src/email/service.ts
+++ Apps/driver-parking/src/email/service.ts
@@ -20,3 +20,23 @@ export async function sendPermitPurchaseConfirmation(email: string, name: string
     console.log(error); //logs any error
   }
 }
+
+export async function sendTicketPaymentConfirmation(email: string, name: string) {
+  const mailgun = new Mailgun(FormData);
+  const mg = mailgun.client({
+    username: "api",
+    key: process.env.API_KEY || "API_KEY",
+  });
+  try {
+    const data = await mg.messages.create("tba@email.com", {
+      from: "Virtual Park <tba@email.com>",
+      to: [`${name} <${email}>`],
+      subject: "Payment Confirmation",
+      text: "Thank you for your payment! Here are the details of your transaction:",
+    });
+
+    console.log(data); // logs response data
+  } catch (error) {
+    console.log(error); //logs any error
+  }
+}
