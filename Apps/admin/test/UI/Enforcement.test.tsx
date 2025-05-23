961680f97d290c887ed8040d4f9c4ccce3b14575
Matthew Solomon <fsolomon@ucsc.edu>
2025-05-22 21:39:48 -0700

    2 basic tests to cover opening and closing the modal #32

--- Apps/admin/test/UI/Enforcement.test.tsx
+++ Apps/admin/test/UI/Enforcement.test.tsx
@@ -1,9 +1,10 @@
-import { afterEach, it, vi } from 'vitest'
-import { cleanup, render, screen } from '@testing-library/react'
+import { afterEach, expect, it, vi } from 'vitest'
+import { cleanup, fireEvent, render, screen } from '@testing-library/react'
 
 import { testEnforcers } from '../MockData'
 import Page from '@/app/enforcement/page'
 import EnforcementList from '@/app/enforcement/EnforcementList'
+import userEvent from '@testing-library/user-event'
 
 const mockPush = vi.fn()
 const mockPathname = "/enforcement"
@@ -33,4 +34,23 @@ it('Renders the list of enforcement officers', async () => {
   render(<EnforcementList />)
   await screen.findByText('Edna Enforcer')
   await screen.findByText('Peter Patrol')
+})
+
+it('Opens the creation modal', async () => {
+  render(<EnforcementList />)
+  await screen.findByText('Edna Enforcer')
+  const addButton = await screen.findByText(/Add new enforcement officer/)
+  fireEvent.click(addButton)
+  await screen.findByText('Submit')
+})
+
+it('Closes the creation modal when clicking away from it', async () => {
+  render(<EnforcementList />)
+  await screen.findByText('Edna Enforcer')
+  const addButton = await screen.findByText(/Add new enforcement officer/)
+  fireEvent.click(addButton)
+  await screen.findByText('Submit')
+  await userEvent.keyboard(`{Escape}`) 
+  const submit = screen.queryByText('Submit')
+  expect(submit).toBeNull()
 })
