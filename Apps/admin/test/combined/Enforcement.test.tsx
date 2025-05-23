883e675be75be889d90daa245b31fae8a52e87b9
Matthew Solomon <fsolomon@ucsc.edu>
2025-05-23 01:08:00 -0700

    Full coverage on actions for creating new enforcement #32

--- Apps/admin/test/combined/Enforcement.test.tsx
+++ Apps/admin/test/combined/Enforcement.test.tsx
@@ -1,8 +1,9 @@
-import { it, afterEach, vi, beforeEach } from 'vitest' 
-import { render, screen, cleanup } from '@testing-library/react'
+import { it, afterEach, vi, beforeEach, expect } from 'vitest' 
+import { render, screen, cleanup, fireEvent, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
 import EnforcementList from '@/app/enforcement/EnforcementList'
-import { testEnforcers } from '../MockData'
+import { newEnforcer, testEnforcers } from '../MockData'
 
 const mockedPush = vi.fn()
 vi.mock('next/navigation', () => ({
@@ -44,4 +45,99 @@ it('Fetch the correct list of enforcers', async () => {
   render(<EnforcementList />)
   await screen.findByText('Edna Enforcer')
   await screen.findByText('Peter Patrol')
+})
+
+it('Creates a new enforcement account successfully', async () => {
+  mockedGetCookies.mockReturnValue({ value: 'mock-session-token' })
+  const { password, ...rest } = newEnforcer
+
+  vi.mocked(fetch).mockImplementation((url, options) => {
+    if (url?.toString().includes('/enforcement')) {
+      if (options?.method == 'POST') {
+        return Promise.resolve({
+          status: 201,
+          json: () => Promise.resolve(rest),
+        } as Response)
+      }
+
+      if (options?.method == 'GET') {
+        return Promise.resolve({
+          status: 200,
+          json: () => Promise.resolve(testEnforcers)
+        } as Response)
+      }
+    }
+
+    return Promise.reject('Unknown fetch')
+  })
+
+  render(<EnforcementList />)
+  let alertCalled = false
+  window.alert = () => (alertCalled = true)
+  await screen.findByText('Edna Enforcer')
+  const addButton = await screen.findByText(/Add new enforcement officer/)
+  fireEvent.click(addButton)
+  await screen.findByText('Submit')
+
+  const nameInput = await screen.findByLabelText(/New Officer Name/)
+  const idInput = await screen.findByLabelText(/New Officer ID/)
+  const emailInput = await screen.findByLabelText(/New Officer Email/)
+  const passwordInput = await screen.findByLabelText(/Set Password/)
+  const confirmInput = await screen.findByLabelText(/Confirm Password/)
+  await userEvent.type(nameInput, newEnforcer.name)
+  await userEvent.type(idInput, newEnforcer.enforcementId)
+  await userEvent.type(emailInput, newEnforcer.email)
+  await userEvent.type(passwordInput, newEnforcer.password)
+  await userEvent.type(confirmInput, newEnforcer.password)
+  fireEvent.click(screen.getByText('Submit'))
+
+  await screen.findByText(newEnforcer.name)
+})
+
+it('Actions handles an undefined response', async () => {
+  mockedGetCookies.mockReturnValue({ value: 'mock-session-token' })
+
+  vi.mocked(fetch).mockImplementation((url, options) => {
+    if (url?.toString().includes('/enforcement')) {
+      if (options?.method == 'POST') {
+        return Promise.resolve({
+          status: 409,
+          json: () => Promise.resolve(undefined),
+        } as Response)
+      }
+
+      if (options?.method == 'GET') {
+        return Promise.resolve({
+          status: 200,
+          json: () => Promise.resolve(testEnforcers)
+        } as Response)
+      }
+    }
+
+    return Promise.reject('Unknown fetch')
+  })
+
+  render(<EnforcementList />)
+  let alertCalled = false
+  window.alert = () => (alertCalled = true)
+  await screen.findByText('Edna Enforcer')
+  const addButton = await screen.findByText(/Add new enforcement officer/)
+  fireEvent.click(addButton)
+  await screen.findByText('Submit')
+
+  const nameInput = await screen.findByLabelText(/New Officer Name/)
+  const idInput = await screen.findByLabelText(/New Officer ID/)
+  const emailInput = await screen.findByLabelText(/New Officer Email/)
+  const passwordInput = await screen.findByLabelText(/Set Password/)
+  const confirmInput = await screen.findByLabelText(/Confirm Password/)
+  await userEvent.type(nameInput, newEnforcer.name)
+  await userEvent.type(idInput, newEnforcer.enforcementId)
+  await userEvent.type(emailInput, newEnforcer.email)
+  await userEvent.type(passwordInput, newEnforcer.password)
+  await userEvent.type(confirmInput, newEnforcer.password)
+  fireEvent.click(screen.getByText('Submit'))
+
+  await waitFor(() => {
+    expect(alertCalled).toBe(true)
+  })
 })
