df63e1e008f99883fefef1bd456246e23ff6ec8b
Jennifer <jchen566@ucsc.edu>
2025-05-23 22:52:13 -0700

    add vehicle service to enforcement #123

---
+++ Apps/enforcement/src/vehicle/service.ts
@@ -0,0 +1,38 @@
+import { Vehicle } from "."
+
+export async function getVehicleByPlate(cookie: string|undefined, plate: string): Promise<Vehicle>  {
+  return new Promise((resolve, reject) => {
+    fetch('http://localhost:4020/graphql', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${cookie}`,
+      },
+      body: JSON.stringify({
+        query: `
+          query ($plate: String!) {
+            getVehicleByPlate(plate: $plate) {
+              id
+              licensePlate
+              driver
+              make
+              model
+              color
+            }
+          }
+        `,
+        variables: { plate: plate },
+      }),
+    })
+    .then(response => {
+      if (response.status != 200) {
+        reject('Unauthorized')
+      }
+      return response.json()} 
+    )
+    .then(json => {
+      resolve(json.data.getVehicleByPlate)
+    })
+    .catch((error) => reject(error))
+  })
+}
