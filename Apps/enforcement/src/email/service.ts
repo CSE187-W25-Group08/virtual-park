97cc82c92eba1fec7403fe7eab5533936fef9244
Jennifer <jchen566@ucsc.edu>
2025-05-23 22:53:53 -0700

    add get driver service and update email contents #123

--- Apps/enforcement/src/email/service.ts
+++ Apps/enforcement/src/email/service.ts
@@ -10,18 +10,18 @@ export async function sendTicketNotification(email: string, name: string, ticket
     key: process.env.MAILGUN_API_KEY || "API_KEY",
   });
   try {
+    const issueDate = new Date(ticket.issue);
+    const dueDate = new Date(ticket.due);
     const data = await mg.messages.create("virtual-park.net", {
       from: "Virtual Park Enforcement <postmaster@virtual-park.net>",
       to: [`${name} <${email}>`],
       subject: "New Ticket",
-      text: `You have been issued a new ticket. Here are the details:\n
-      Lot: ${ticket.lot}\n
-      Vehicle: ${ticket.vehicle}\n
+      text: `You have been issued a new ticket:\n
       Description: ${ticket.description}\n
       Violation: ${ticket.violation}\n
-      Issue Date: ${ticket.issue}\n
-      Due Date: ${ticket.due}\n
-      Cost: ${ticket.cost}\n
+      Issue Date: ${issueDate}\n
+      Due Date: ${dueDate}\n
+      Cost: $${ticket.cost}\n
       Please log in to your account to view more details.`,
     });
 
