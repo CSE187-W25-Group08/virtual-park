17929ae977c169f1d1df396bca6fdad0c77e7745
Jin Le <jiqle@ucsc.edu>
2025-05-24 07:19:38 -0700

    testing repdeployment and deleted unnecessary comment in stripe page

--- Apps/driver-parking/src/stripe/service.ts
+++ Apps/driver-parking/src/stripe/service.ts
@@ -1,40 +1,4 @@
 
-/*
-export async function getClientSecretService(
-  cookie: string | undefined,
-  amount: number
-): Promise<string> {
-  return new Promise((resolve, reject) => {
-    fetch("http://localhost:4060/graphql", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${cookie}`,
-      },
-      body: JSON.stringify({
-        query: `      
-        mutation {
-          createPaymentIntent(amount: ${amount}) {
-          clientSecret
-        }
-      }`,
-      }),
-    })
-      .then((response) => {
-        if (response.status != 200) {
-          return reject("Error getting client secret service");
-        }
-
-        return response.json();
-      })
-      .then((json) => {
-        console.log(json);
-        resolve(json.data.createPaymentIntent.clientSecret);
-      })
-      .catch((error) => reject(error));
-  });
-}
-*/
 
 export async function getCheckoutSessionUrl(
   cookie: string | undefined,
