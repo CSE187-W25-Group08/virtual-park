2c0a88b32bb4878b2e0a04aa95a5cd793a060d51
Bodie <bfeinber@ucsc.edu>
2025-05-24 15:29:45 -0700

    docker container in CI test attempt 2

--- Apps/admin/src/app/lots/action.ts
+++ Apps/admin/src/app/lots/action.ts
@@ -2,5 +2,7 @@ import { Lot } from "../../lot";
 import { LotService } from "../../lot/service";
 
 export async function getLots(): Promise<Lot[]> {
-  return new LotService().getLots()
+  const lots = new LotService().getLots();
+  console.log("admin lot action get lots", lots);
+  return lots;
 }
