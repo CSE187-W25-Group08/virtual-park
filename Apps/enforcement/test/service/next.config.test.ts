5661ea052c217024c10497dad517f5492d0d5ccf
Jennifer <jchen566@ucsc.edu>
2025-05-24 17:49:29 -0700

    add timeout on long ticket tests #123

--- Apps/enforcement/test/service/next.config.test.ts
+++
@@ -1,7 +0,0 @@
-import { it, expect, vi, beforeEach } from 'vitest';
-import nextConfig from '../../next.config'
-
-
-it('exports a NextConfig object', () => {
-  expect(typeof nextConfig).toBe('object')
-})
