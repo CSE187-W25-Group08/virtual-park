1ac2ee619e82816e181feae0ccbdc588efac0d52
Hamza <hbassal@ucsc.edu>
2025-05-29 12:38:27 -0700

    yet another lint fix

--- Apps/enforcement/src/app/ticket/action.ts
+++ Apps/enforcement/src/app/ticket/action.ts
@@ -8,7 +8,7 @@ import { Lot } from '../../lot'
 import { issueTicketForVehicle } from '../../ticket/service'
 import { getAllLots } from '../../lot/service'
 import { getDriver } from '../../auth/service'
-import { Driver } from '../../auth'
+// import { Driver } from '../../auth'
 import { sendTicketNotification } from '../../email/service'
 
 export async function issueTicketForCar(
