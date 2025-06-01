36d5697ac742ff87cfa384774106cb7793619802
Bodie <bfeinber@ucsc.edu>
2025-05-31 18:56:31 -0700

    factorized mobile landing page #126

--- Apps/driver-parking/src/app/landing/LotList.tsx
+++ Apps/driver-parking/src/app/landing/LotList.tsx
@@ -1,28 +1,13 @@
 'use client';
-
 import { useEffect, useState } from 'react';
-import { TextField, Autocomplete, ListItemIcon, ListItem, ListItemText, Chip} from '@mui/material';
-import LocalParkingIcon from '@mui/icons-material/LocalParking';
+import { Box, TextField, Autocomplete, ListItemIcon, ListItem, ListItemText} from '@mui/material';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
-import MapIcon from '@mui/icons-material/Map';
-import VerifiedUserIcon from '@mui/icons-material/VerifiedUser';
 import { getLots } from '../[locale]/lot/action';
 import { Lot } from '@/lot';
-
-import {
-  Box,
-  Card,
-  CardContent,
-  CardActions,
-  Typography,
-  Button,
-  Divider,
-} from '@mui/material';
-import { useRouter } from 'next/navigation'
+import LotDetails from './LotDetails';
 export default function LotList() {
   const [lots, setLots] = useState<Lot[]>([]);
   const [selectedLot, setSelectedLot] = useState<Lot | null>(null);
-  const router = useRouter()
 
   useEffect(() => {
     const setLotData = async () => {
@@ -57,51 +42,7 @@ export default function LotList() {
         noOptionsText="No lots found"
         fullWidth
       />
-
-      {selectedLot && (
-        <Box mt={4} display="flex" justifyContent="center">
-          <Card sx={{ maxWidth: 400, width: '100%', p: 2, borderRadius: 3, boxShadow: 4 }}>
-            <CardContent>
-              <Typography variant="h6" gutterBottom>
-                <Box display="flex" alignItems="center">
-                  <LocalParkingIcon fontSize="medium" sx={{ mr: 1 }} />
-                  {selectedLot.name}
-                </Box>
-              </Typography>
-              <Divider sx={{ mb: 1 }} />
-
-              <Typography variant="subtitle2" sx={{ display: 'flex', alignItems: 'center', mb: 1 }}>
-                {selectedLot.address}
-              </Typography>
-
-              <Typography variant="subtitle1" sx={{ display: 'flex', alignItems: 'center', mb: 1 }}>
-                <MapIcon fontSize="small" sx={{ mr: 1 }} />
-                <strong>Zone:</strong>&nbsp;{selectedLot.zone}
-              </Typography>
-
-              <Typography variant="subtitle1" sx={{ display: 'flex', alignItems: 'center' }}>
-                <Box sx={{display:'flex', flexDirection:'column'}}>
-                  <Box sx={{display:'flex', alignItems:'center', mb: 1}}>
-                    <VerifiedUserIcon fontSize="small" sx={{ mr: 1 }} />
-                    <strong>Valid Permits:</strong>
-                  </Box>
-                  <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 1 }}>
-                    {selectedLot.validPermits.map((permit: string) => (
-                      <Chip key={permit} label={permit} color="info" variant="outlined" />
-                    ))}
-                  </Box>
-                </Box>
-              </Typography>
-            </CardContent>
-
-            <CardActions sx={{ justifyContent: 'flex-end' }}>
-              <Button variant="contained" color="primary" onClick={() => router.push('/permit/purchase')}>
-                Buy Permit
-              </Button>
-            </CardActions>
-          </Card>
-        </Box>
-      )}
+      {selectedLot && (<LotDetails selectedLot={selectedLot} /> )}
     </Box>
   );
 }
