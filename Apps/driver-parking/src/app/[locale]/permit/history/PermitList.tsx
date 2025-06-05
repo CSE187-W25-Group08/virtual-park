1aab6aea3186da8d690972401b0c94bd03af2392
Matthew Solomon <fsolomon@ucsc.edu>
2025-06-05 02:57:11 -0700

    Card for empty permit history page

--- Apps/driver-parking/src/app/[locale]/permit/history/PermitList.tsx
+++ Apps/driver-parking/src/app/[locale]/permit/history/PermitList.tsx
@@ -1,17 +1,21 @@
 'use client'
 
 import Box from '@mui/material/Box'
+import Card from '@mui/material/Card'
 import Typography from '@mui/material/Typography'
+import HourglassEmptyIcon from '@mui/icons-material/HourglassEmpty'
 import { useState, useEffect } from 'react'
 import { useTranslations } from 'next-intl'
 
 import PermitListCard from './PermitListCard'
 import { getUserPermits } from '../actions'
 import { Permit } from '../../../../permit'
+import { useTheme } from '@mui/material'
 
 export default function PermitList() {
   const [permits, setPermits] = useState<Permit[]>([])
   const t = useTranslations('permit_history')
+  const theme = useTheme()
 
   useEffect(() => {
     console.log("permit history useEffect");
@@ -37,6 +41,25 @@ export default function PermitList() {
         {permits && permits.map((permit, index) => (
           <PermitListCard key={index} permit={permit} />
         ))}
+        {permits.length == 0 && (
+        <Box>
+          <Card 
+            sx={{ 
+              borderRadius: 3,
+              boxShadow: 2,
+              p: 2,
+              border: '1px solid #ccc',
+              width: '100%'
+            }}>
+              <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+                  <HourglassEmptyIcon sx={{ color: theme.palette.secondary.main }} />
+                  <Typography variant='h6'>{t('noHistory')}</Typography>
+                </Box>
+              </Box>
+            </Card>
+          </Box>
+        )}
       </Box>
     </Box>
   );
