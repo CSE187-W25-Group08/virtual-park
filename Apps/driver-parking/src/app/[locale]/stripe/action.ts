69246fce8f188a544c5e9b1099f8bdd1fdb8ae23
Matthew Solomon <fsolomon@ucsc.edu>
2025-06-05 11:15:37 -0700

    Removed unused code that wont be reused

--- Apps/driver-parking/src/app/[locale]/stripe/action.ts
+++ Apps/driver-parking/src/app/[locale]/stripe/action.ts
@@ -3,14 +3,6 @@
 import { getCheckoutSessionUrl } from "../../../stripe/service";
 import { cookies } from 'next/headers'
 
-
-/*
-export async function getClientSecretAction(amount : number): Promise<string> {
-  const cookie = (await cookies()).get('session')?.value
-  return await getClientSecretService(cookie, amount);
-}
-*/
-
 export async function getCheckoutSessionUrlAction(amount : number, name: string, metadata: Record<string, unknown>, successUrl: string, cancelUrl: string): Promise<string> {
   const cookie = (await cookies()).get('session')?.value
   const sessionUrl = await getCheckoutSessionUrl(cookie, amount, name, metadata, successUrl, cancelUrl);
