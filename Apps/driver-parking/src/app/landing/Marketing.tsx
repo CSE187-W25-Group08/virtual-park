4d93f75f4d8c850784c937b4e566a06a77373de2
Jennifer <jchen566@ucsc.edu>
2025-06-05 19:44:45 -0700

    add login/signup/logout buttons to desktop landing #180

--- Apps/driver-parking/src/app/landing/Marketing.tsx
+++ Apps/driver-parking/src/app/landing/Marketing.tsx
@@ -1,20 +1,59 @@
 "use client";
 
-import { AppBar, Box, IconButton, Toolbar, Typography } from "@mui/material";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { 
+  AppBar,
+  Box,
+  IconButton,
+  Toolbar,
+  Typography,
+  Menu,
+  MenuItem,
+  ListItemIcon,
+  ListItemText
+} from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
-import SearchIcon from "@mui/icons-material/Search";
+import LoginIcon from "@mui/icons-material/Login";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import LogoutIcon from "@mui/icons-material/Logout";
 import Image from "next/image";
+import { useTranslations } from "next-intl";
+import { useTheme, Theme } from "@mui/material/styles";
+
 import logo from "../public/img/sprite-logo.svg";
 import hoody from "../public/img/Front.png";
 import marketcard from "../public/img/300.png";
 import cat from "../public/img/bg-desktop.png";
-import { useTranslations } from "next-intl";
-
-import { useTheme, Theme } from "@mui/material/styles";
 import LotList from "./LotList";
+import { logout } from "../[locale]/login/action";
+
 export default function Marketing() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+  const router = useRouter();
   const t = useTranslations("landing");
   const theme = useTheme<Theme>();
+
+  useEffect(() => {
+    const session = window.sessionStorage.getItem('name')
+    setIsAuthenticated(!!session)
+  }, [])
+
+  const handleMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleMenuClose = () => {
+    setAnchorEl(null)
+  }
+
+  const handleLogout = async () => {
+    await logout()
+    window.sessionStorage.clear()
+    window.location.reload()
+  }
+
   return (
     <Box>
       <AppBar
@@ -30,14 +69,58 @@ export default function Marketing() {
               Virtual Park
             </Typography>
           </Box>
-          {/* Search and Menu Icons */}
+          {/* Menu Icon */}
           <Box display="flex" alignItems="center" gap={1}>
-            <IconButton edge="end" color="inherit">
-              <SearchIcon />
-            </IconButton>
-            <IconButton edge="end" color="inherit">
+            <IconButton
+              edge="end"
+              color="inherit"
+              onClick={handleMenuOpen}
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              aria-label="menu options"
+            >
               <MenuIcon />
             </IconButton>
+
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              keepMounted
+              open={Boolean(anchorEl)}
+              onClose={handleMenuClose}
+              disableScrollLock
+              anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+              transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+              PaperProps={{
+                sx: {
+                  maxWidth: '90vw',
+                  width: '200px'
+                }
+              }}
+            >
+              {!isAuthenticated && [
+                <MenuItem key="login" onClick={() => { router.push('/login'); handleMenuClose(); }}>
+                  <ListItemIcon>
+                    <LoginIcon fontSize="small" />
+                  </ListItemIcon>
+                  <ListItemText>{t('login')}</ListItemText>
+                </MenuItem>,
+                <MenuItem key="signup" onClick={() => { router.push('/signup'); handleMenuClose(); }}>
+                  <ListItemIcon>
+                    <PersonAddIcon fontSize="small" />
+                  </ListItemIcon>
+                  <ListItemText>{t('signup')}</ListItemText>
+                </MenuItem>
+              ]}
+              {isAuthenticated && (
+                <MenuItem onClick={() => { handleLogout(); handleMenuClose(); }}>
+                  <ListItemIcon>
+                    <LogoutIcon fontSize="small" />
+                  </ListItemIcon>
+                  <ListItemText>{t('logout')}</ListItemText>
+                </MenuItem>
+              )}
+            </Menu>
           </Box>
         </Toolbar>
       </AppBar>
