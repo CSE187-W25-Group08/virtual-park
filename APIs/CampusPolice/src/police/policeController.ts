51650b8555382dc96463fcba672d112209d074c1
Matthew Solomon <fsolomon@ucsc.edu>
2025-06-06 03:15:27 -0700

    Added police endpoint to get T/F for if a license plate has a permit #176

--- APIs/CampusPolice/src/police/policeController.ts
+++ APIs/CampusPolice/src/police/policeController.ts
@@ -4,10 +4,12 @@ import {
   Route,
   SuccessResponse,
   Get,
-  Security
+  Security,
+  Query
 } from 'tsoa'
-import { hasValidAPIKey } from './policeService';
 
+import { PoliceService } from './policeService'
+import { Permit } from '.'
 
 @Route('police')
 @Security('api_key') 
@@ -17,6 +19,29 @@ export class PoliceController extends Controller {
   @Response('401', 'Unauthorized')
   @SuccessResponse('200', 'API Key Success')
   public async testKey(): Promise<string> {
-    return await hasValidAPIKey()
+    const response = await new PoliceService().hasValidAPIKey()
+    return response
   }
+
+  @Get('/permit')
+  @Response('401', 'Unauthorized')
+  @SuccessResponse('200', 'Permit Response')
+  public async checkPermit(
+    @Query() licensePlate: string
+  ): Promise<boolean> {
+    return new PoliceService().checkPermitFromPlate(licensePlate)
+      .then(async (permits: Permit[]): Promise<boolean> => {
+
+        if (permits.length > 0) {
+          const validPermit = permits.filter(permit => permit.isValid)
+          if (validPermit.length > 0) {
+            return true
+          }
+          return false
+        }
+
+        return false
+      })
+  }
+
 }
--- APIs/CampusPolice/src/police/policeService.ts
+++ APIs/CampusPolice/src/police/policeService.ts
@@ -1,7 +1,45 @@
-export async function hasValidAPIKey(): Promise<string> {
-    try {
-      return 'this works'
-    } catch {
-      return 'catch err';
-    }
+import { Permit } from '.';
+
+export class PoliceService {
+  public async hasValidAPIKey(): Promise<string> {
+      try {
+        return 'this works'
+      } catch {
+        return 'catch err';
+      }
+  }
+
+  public async checkPermitFromPlate(licensePlate: string): Promise<Permit[]> {
+    return new Promise((resolve, reject) => {
+      fetch('http://localhost:4000/graphql', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${process.env.POLICE_API_KEY}`,
+        },
+        body: JSON.stringify({
+          query: `
+            query GetPermitByCar($input: String!) {
+              getPermitBycarPlate(input: $input) {
+                isValid
+              }
+            }
+          `,
+          variables: {
+            input: licensePlate,
+          },
+        }),
+      })
+      .then(response => {
+        if (response.status !== 200) {
+          reject('Unauthorized')
+        }
+        return response.json()
+      })
+      .then(json => {
+        resolve(json.data.getPermitBycarPlate)
+      })
+      .catch((error) => reject(error))
+    })
+  }
 }
